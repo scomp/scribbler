3f46047373d9b3a17255aea7a8eabd740fadd439
scomp <jemduff@gmail.com>
2019-02-12 21:34:02 +1100

    COOL

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ extern crate font8x8;
 use std::time::{Duration, Instant};
 
 use font8x8::legacy::BASIC_LEGACY; 
+use font8x8::legacy::CONTROL_LEGACY; 
+
 //extern crate png;
 
 //use std::fs::File;
+
+const MIX_CHANNELS: usize = 26;
+
 
 use minifb::{CursorStyle, MouseButton, MouseMode, Window, Key, Scale, WindowOptions};
 
@@ -17,23 +22,37 @@ struct TextBlock {
   text: Vec<u8>,
   x: usize,
   y: usize,
+  right: usize,
+  bottom: usize,
 }
 
 impl TextBlock {
   fn new(text: String, x: usize, y: usize) -> TextBlock {
-    TextBlock{ text: text.as_bytes().to_vec(), x: x , y: y}
+    TextBlock{ text: text.as_bytes().to_vec(), x: x , y: y, right: x + text.len() * 8  , bottom :y+8 }
   }
 }
 
-fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    let mut mouseBuffer: Vec<usize> = vec![0;100];
+
+
+
+fn main() {
+
+    let mut collision: Vec<usize> = vec![0; 100];
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut channel_buffers: Vec<u32> = vec![0; 56 * 128 * 14];
+    let mut mouse_buffer: Vec<usize> = vec![0;100];
 
     let mut text_blocks: Vec<TextBlock> = vec![];
 
-    text_blocks.push( TextBlock::new( "HELLO".to_string(), 100, 100));
-    text_blocks.push( TextBlock::new( "VECTORS".to_string(), 20, 20));  
+    text_blocks.push( TextBlock::new( "FILE".to_string(), 0,0));
+
+    // for x in (0..26) {
+    //     text_blocks.push( TextBlock::new( format!("CHAN {}",x), x*8*8+36, 200));
+    // }
+
+    
     let mut window = match Window::new("Scriblr - Press ESC to exit", WIDTH, HEIGHT,
                                        WindowOptions {
                                            scale: Scale::X1,
@@ -48,24 +67,53 @@ fn main() {
     let size = 5;
 
     let mut mouse_down = false;
+    let mut action = true;
+    let mut menup = false;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        let mut action = false;
+        
         let start = Instant::now();
+
         window.get_mouse_pos(MouseMode::Discard).map(|mouse| {
             let screen_pos = ((mouse.1 as usize) * WIDTH) + mouse.0 as usize;
             //println!("{:?}", window.get_unscaled_mouse_pos(MouseMode::Discard).unwrap());
 
             if window.get_mouse_down(MouseButton::Left) {
               // start buffer?
+
+              // am I inside the block?
+              
+              for b in text_blocks.iter() {
+                if (mouse.0 as usize) >= b.x && (mouse.0 as usize) <= b.right
+                  && (mouse.1 as usize) >= b.y && (mouse.1 as usize) <= b.bottom{
+                    menup = true;
+                    println!("menu should be up");
+                  }else{
+                    menup = false;
+                  }
+              }
+
               if ! mouse_down {
-                mouseBuffer.clear();
+                mouse_buffer.clear();
                 mouse_down = true;
               }
 
-              text_blocks[0].text = format!("{}{}",mouse.0, mouse.1).as_bytes().to_vec();
+              // if menup {
+              //   text_blocks.push( TextBlock::new( "NEW".to_string(), 0,8));
+              //   text_blocks.push( TextBlock::new( "SAVE".to_string(), 0,16));
+              //   text_blocks.push( TextBlock::new( "SAVE AS ...".to_string(), 0,16));
+              //   text_blocks.push( TextBlock::new( "EXIT".to_string(), 0,24));
+              // }else{
+              //   text_blocks.pop();
+              //   text_blocks.pop();
+              //   text_blocks.pop();
+              //   text_blocks.pop();
+              // }
 
-              mouseBuffer.push(screen_pos);
+              // text_blocks[0].text = format!("{:.*}",2,mouse.0).as_bytes().to_vec();
+              // text_blocks[1].text = format!("{:.*}",2,mouse.1).as_bytes().to_vec();
+
+              mouse_buffer.push(screen_pos);
               // if window.is_key_down(Key::LeftShift) {
               //   buffer[screen_pos] = 0;
               // }else {
@@ -80,15 +128,15 @@ fn main() {
               //     buffer[screen_pos-(WIDTH*n)-m] = 0x00ffffff;
               //   }
               // }
-              action = true;
+              //action = true;
           
               window.set_cursor_style(CursorStyle::Crosshair);
               
             }else{ // handle mouse up
               if mouse_down {
-                text_blocks[1].text = format!("VECTOR LENGTH {}",&mouseBuffer.len()).as_bytes().to_vec();
+                //text_blocks[1].text = format!("VECTOR LENGTH {}",&mouse_buffer.len()).as_bytes().to_vec();
 
-                for x in &mouseBuffer {
+                for x in &mouse_buffer {
                   buffer[*x]   = 0x00ff0000;
                 }
               }
@@ -96,7 +144,18 @@ fn main() {
             if window.get_mouse_down(MouseButton::Right) {
               buffer[screen_pos] = 0;
             }
-            for tb in text_blocks.iter() {
+            
+        });
+
+        // window.get_scroll_wheel().map(|scroll| {
+        //     println!("Scrolling {} - {}", scroll.0, scroll.1);
+        // });
+
+        // We unwrap here as we want this code to exit if it fails
+        //window.update_with_buffer(&buffer).unwrap();
+        //buffer.
+        if action {
+          for tb in text_blocks.iter() {
                 let width_offset = (WIDTH * tb.y) + tb.x;
                 //println!("{}",tb.text.to_string());
                 for itd in 0..8 {
@@ -115,16 +174,6 @@ fn main() {
                   }
                 }
               }
-        });
-
-        // window.get_scroll_wheel().map(|scroll| {
-        //     println!("Scrolling {} - {}", scroll.0, scroll.1);
-        // });
-
-        // We unwrap here as we want this code to exit if it fails
-        window.update_with_buffer(&buffer).unwrap();
-        //buffer.
-        if action {
           window.update_with_buffer(&buffer).unwrap();
           println!("RenderLOOP took {:?}", start.elapsed().as_micros());
         }        
